3f5e4ccbba577a3ebf5b75a8de55956105e91557
Tanveer <tanveera915637@gmail.com>
2025-07-11 17:41:04 +0600

    Designing Menu Item

--- src/component/body/MenuItem.js
+++ src/component/body/MenuItem.js
@@ -1,9 +1,20 @@
 import React from 'react'
-
-const MenuItem = ({dish}) => {
-  return (
-    <div>{dish.name}</div>
-  )
+import { Card, CardGroup } from 'react-bootstrap'
+const MenuItem = ({ dish }) => {
+    return (
+            <Card border='dark' style={{padding:4, margin:10}}>
+                <Card.Img variant="top" src={dish.image} />
+                <Card.Body>
+                    <Card.Title style={{fontSize:30, fontWeight:'bold'}}>{dish.name}</Card.Title>
+                    <Card.Text style={{color:'green'}} >
+                        {dish.description}
+                    </Card.Text>
+                </Card.Body>
+                <Card.Footer>
+                    <small>Price: {dish.price}</small>
+                </Card.Footer>
+            </Card>
+    )
 }
 
 export default MenuItem
