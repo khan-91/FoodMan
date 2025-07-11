7612a69b5e6d19f58d059ac5d455e69eae7b1b2f
Tanveer <tanveera915637@gmail.com>
2025-07-11 20:19:38 +0600

    Desiging Nevigation bar

--- src/component/header/NavigationComponent.js
+++ src/component/header/NavigationComponent.js
@@ -3,30 +3,30 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
+
 
 
 const NavigationComponent = () => {
     return (
         <div>
-            <Navbar expand="lg" className="nav-bar" bg="dark" variant='dark'>
+            <Navbar expand="sm" className="nav-bar" bg="dark" variant='dark'>
                 <Container>
-                    <Navbar.Brand href="#home">FoodMan</Navbar.Brand>
+                    <Navbar.Brand href="/">FoodMan</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
-                            <Nav.Link href="#home">Home</Nav.Link>
-                            <Nav.Link href="#link">Link</Nav.Link>
-                            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.2">
-                                    Another action
-                                </NavDropdown.Item>
-                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                <NavDropdown.Divider />
-                                <NavDropdown.Item href="#action/3.4">
-                                    Separated link
-                                </NavDropdown.Item>
-                            </NavDropdown>
+                            {/* <Nav.Link href="#home">Home</Nav.Link> */}
+                            {/* <Nav.Link href="#link">Menu</Nav.Link> */}
+                            {/* <Link to="/">Home</Link>
+                            <Link to="/menu">Menu</Link>
+                            <Link to="/about">About</Link>
+                            <Link to="/Contact">Contact</Link> */}
+                            {/* <Nav.Link href="#link">Contact</Nav.Link> */}
+                            <Nav.Link as={Link} to="/">Home</Nav.Link>
+                            <Nav.Link as={Link} to="/menu">Menu</Nav.Link>
+                            <Nav.Link as={Link} to="/about">About</Nav.Link>
+                            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
