9759cc53686e0f884b7e53690c8601d6d07b9c0f
Tanveer <tanveera915637@gmail.com>
2025-07-11 19:31:35 +0600

    Preparing For Router

--- src/component/body/BodyComponent.js
+++ src/component/body/BodyComponent.js
@@ -1,10 +1,10 @@
 import React from 'react'
-import MenuComponent from './MenuComponent'
+
 
 const BodyComponent = () => {
   return (
     <div>
-      <MenuComponent />
+      
     </div>
   )
 }
