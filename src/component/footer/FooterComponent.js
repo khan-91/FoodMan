35c82342e81a770003e1f7d83edcd5ce6c84a08a
Tanveer <tanveera915637@gmail.com>
2025-07-12 14:20:37 +0600

    Footer Component Add

--- src/component/footer/FooterComponent.js
+++ src/component/footer/FooterComponent.js
@@ -1,8 +1,18 @@
 import React from 'react'
+import { Container } from 'react-bootstrap'
 
 const FooterComponent = () => {
   return (
-    <div>FooterComponent</div>
+    <div className="footer">
+      <div className='container'>
+        <div className='row justify-content-center'>
+          <div className='col-auto'>
+            <p>FoodMan &#169; Copyright 2025 </p>
+
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
